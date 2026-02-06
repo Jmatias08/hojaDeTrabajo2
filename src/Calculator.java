a7e6b93bb931858a104b299c3ec123d789dec910
DostynL <dostynlpz@gmail.com>
2026-02-06 10:32:50 -0600

    clase calculadora

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,43 @@
-public class Calculator {
-    
+/**
+ * ADT Calculadora.
+ * Define las operaciones aritméticas básicas.
+ */
+public interface Calculator {
+
+    /**
+     * Suma dos enteros.
+     *
+     * @param a primer operando
+     * @param b segundo operando
+     * @return resultado de a + b
+     */
+    int sumar(int a, int b);
+
+    /**
+     * Multiplica dos enteros.
+     *
+     * @param a primer operando
+     * @param b segundo operando
+     * @return resultado de a * b
+     */
+    int multiplicar(int a, int b);
+
+    /**
+     * Divide dos enteros.
+     *
+     * @param a dividendo
+     * @param b divisor
+     * @return resultado de a / b
+     * @throws ArithmeticException si b es cero
+     */
+    int dividir(int a, int b);
+
+    /**
+     * Resta dos enteros.
+     *
+     * @param a primer operando
+     * @param b segundo operando
+     * @return resultado de a - b
+     */
+    int restar(int a, int b);
 }
