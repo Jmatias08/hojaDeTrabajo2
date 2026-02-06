de301632893129a6d34e64bf32bdb95ca0a5d90a
DostynL <dostynlpz@gmail.com>
2026-02-06 10:35:13 -0600

    prueba unitaria

--- src/PostfixCalculatorTest.java
+++ src/PostfixCalculatorTest.java
@@ -1,3 +1,28 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Pruebas unitarias para la calculadora postfix.
+ */
 public class PostfixCalculatorTest {
-    
+
+    /**
+     * Prueba de evaluación correcta.
+     */
+    @Test
+    public void testEvaluate() throws Exception {
+        Stack<Integer> stack = new VectorStack<>();
+        PostfixCalculator calc = new PostfixCalculator(stack);
+        assertEquals(15, calc.evaluate("1 2 + 4 * 3 +"));
+    }
+
+    /**
+     * Prueba de división entre cero.
+     */
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionByZero() throws Exception {
+        Stack<Integer> stack = new VectorStack<>();
+        PostfixCalculator calc = new PostfixCalculator(stack);
+        calc.evaluate("4 0 /");
+    }
 }
